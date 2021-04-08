5e473a52d3fbc7861baf308002493878d2f2618c
Rodrigo Sobral <rodrigosobral@sapo.pt>
2021-04-08 23:55:02 +0100

    ER apresentation done

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# BD_Assignment_2021
- Database Assignment @2021
+# DataBases Assignment 2021
+ 
+## Relationship Entity Diagram
+
+Below we explain how and why we built this RE structure
+
+![Relationship Entity Diagram](screenshots/Screenshot_ERDiagram.png)
+
+:one: Users
+
+We started building users subsystem. 
+
+So we decided to separate `buyers` and `sellers` because both are able to do different things, a `seller`, for example, is able to create several auctions (at leasts one, otherwise he isn't even considered a seller), by other side, `buyer` can bid several auctions (at least one too, for the same reason as the previous one). However, they still being descended entities from `user`, which means a `user` can be simultaneously a `seller` and a `buyer`, that's why, plus redundancy avoidance, whe used a `complete inheritance`.
+
+
+:two: Auction
+
+After that, we implemented `auction` and its involved entities.
+
+An `auction` is created by one and only one `seller` and must have a biding `product` associated. In other words, an `auction` existance doesn't make sense without a `product`, so `product` is a weak entity.
+
+:three: Biddings
+
+Associated to each `auction`, there's a bidding `history`. Is here where every `biddings` are stored and can be consulted later (even there's no one).
+
+Each `bidding` contains the propused value, the `buyer` ID and the corresponding `history` ID, which belongs to a certain `auction`.
+
+:four: Messages & Notifications
+
+One more thing it was proposed, was to build a notification ecosystem relationated to `auctions` and general `users`. 
+
+To do that, we decided to firstly create a `mural` where all `users` can post (or not) several `messages`. Besides that, `buyers` who had an overlaid `bid`, had to (once again, or not) receive a `message` (once again, or several) warning them. So we created an `inbox`, which works like a bridge between `users` and `messages` from `auctions`.
--- SQL script.sql
+++ SQL script.sql
@@ -38,21 +38,14 @@ CREATE TABLE comprador (
 	PRIMARY KEY(user_userid)
 );
 
-CREATE TABLE inbox (
-	user_userid BIGINT,
-	PRIMARY KEY(user_userid)
-);
-
 CREATE TABLE mensagem (
-	remetente		 VARCHAR(128) NOT NULL,
 	conteudo		 VARCHAR(4096) NOT NULL,
 	mural_leilao_leilaoid BIGINT,
+	user_userid		 BIGINT NOT NULL,
 	PRIMARY KEY(mural_leilao_leilaoid)
 );
 
 CREATE TABLE historico (
-	valor_licitado		 FLOAT(8) NOT NULL,
-	comprador_user_userid	 BIGINT,
 	leilao_leilaoid		 BIGINT,
 	leilao_vendedor_user_userid BIGINT,
 	PRIMARY KEY(leilao_leilaoid,leilao_vendedor_user_userid)
@@ -64,10 +57,17 @@ CREATE TABLE mural (
 	PRIMARY KEY(leilao_leilaoid,leilao_vendedor_user_userid)
 );
 
-CREATE TABLE inbox_mensagem (
-	inbox_user_userid		 BIGINT,
+CREATE TABLE licitacao (
+	valor_licitado		 FLOAT(8) NOT NULL,
+	historico_leilao_leilaoid BIGINT,
+	comprador_user_userid	 BIGINT,
+	PRIMARY KEY(historico_leilao_leilaoid,comprador_user_userid)
+);
+
+CREATE TABLE inbox (
 	mensagem_mural_leilao_leilaoid BIGINT,
-	PRIMARY KEY(inbox_user_userid,mensagem_mural_leilao_leilaoid)
+	user_userid			 BIGINT,
+	PRIMARY KEY(user_userid)
 );
 
 ALTER TABLE leilao ADD CONSTRAINT leilao_fk1 FOREIGN KEY (vendedor_user_userid) REFERENCES vendedor(user_userid);
@@ -75,13 +75,14 @@ ALTER TABLE produto ADD CONSTRAINT produto_fk1 FOREIGN KEY (leilao_leilaoid) REF
 ALTER TABLE produto ADD CONSTRAINT produto_fk2 FOREIGN KEY (leilao_vendedor_user_userid) REFERENCES leilao(vendedor_user_userid);
 ALTER TABLE vendedor ADD CONSTRAINT vendedor_fk1 FOREIGN KEY (user_userid) REFERENCES user(userid);
 ALTER TABLE comprador ADD CONSTRAINT comprador_fk1 FOREIGN KEY (user_userid) REFERENCES user(userid);
-ALTER TABLE inbox ADD CONSTRAINT inbox_fk1 FOREIGN KEY (user_userid) REFERENCES user(userid);
 ALTER TABLE mensagem ADD CONSTRAINT mensagem_fk1 FOREIGN KEY (mural_leilao_leilaoid) REFERENCES mural(leilao_leilaoid);
-ALTER TABLE historico ADD CONSTRAINT historico_fk1 FOREIGN KEY (comprador_user_userid) REFERENCES comprador(user_userid);
-ALTER TABLE historico ADD CONSTRAINT historico_fk2 FOREIGN KEY (leilao_leilaoid) REFERENCES leilao(leilaoid);
-ALTER TABLE historico ADD CONSTRAINT historico_fk3 FOREIGN KEY (leilao_vendedor_user_userid) REFERENCES leilao(vendedor_user_userid);
+ALTER TABLE mensagem ADD CONSTRAINT mensagem_fk2 FOREIGN KEY (user_userid) REFERENCES user(userid);
+ALTER TABLE historico ADD CONSTRAINT historico_fk1 FOREIGN KEY (leilao_leilaoid) REFERENCES leilao(leilaoid);
+ALTER TABLE historico ADD CONSTRAINT historico_fk2 FOREIGN KEY (leilao_vendedor_user_userid) REFERENCES leilao(vendedor_user_userid);
 ALTER TABLE mural ADD CONSTRAINT mural_fk1 FOREIGN KEY (leilao_leilaoid) REFERENCES leilao(leilaoid);
 ALTER TABLE mural ADD CONSTRAINT mural_fk2 FOREIGN KEY (leilao_vendedor_user_userid) REFERENCES leilao(vendedor_user_userid);
-ALTER TABLE inbox_mensagem ADD CONSTRAINT inbox_mensagem_fk1 FOREIGN KEY (inbox_user_userid) REFERENCES inbox(user_userid);
-ALTER TABLE inbox_mensagem ADD CONSTRAINT inbox_mensagem_fk2 FOREIGN KEY (mensagem_mural_leilao_leilaoid) REFERENCES mensagem(mural_leilao_leilaoid);
+ALTER TABLE licitacao ADD CONSTRAINT licitacao_fk1 FOREIGN KEY (historico_leilao_leilaoid) REFERENCES historico(leilao_leilaoid);
+ALTER TABLE licitacao ADD CONSTRAINT licitacao_fk2 FOREIGN KEY (comprador_user_userid) REFERENCES comprador(user_userid);
+ALTER TABLE inbox ADD CONSTRAINT inbox_fk1 FOREIGN KEY (mensagem_mural_leilao_leilaoid) REFERENCES mensagem(mural_leilao_leilaoid);
+ALTER TABLE inbox ADD CONSTRAINT inbox_fk2 FOREIGN KEY (user_userid) REFERENCES user(userid);
 
